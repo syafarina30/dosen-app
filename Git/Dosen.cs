52494f321899fba6cdcabc20c96ed3c2db7b2dc8
syafarina30 <syafarinanurul38@gmail.com>
2023-04-10 20:39:01 +0700

    Inisialisasi project Dosen-app

---
+++ Git/Dosen.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dosen
+{ 
+    internal class Dosen
+{
+    public int Id;
+    public string Nama;
+    public int Nik;
+    public string Gender;
+    public string Course;
+
+    public Dosen()
+    {
+        this.Id = 1;
+        this.Nama = "Dosen Pengampu ";
+        this.Nik = 123456789;
+        this.Gender = "Pria ";
+        this.Course = "Pemrograman Berorientasi Objek";
+    }
+
+    public void attDosen(string nama, int nik)
+    {
+        this.Nama = nama;
+        this.Nik = nik;
+    }
+
+    public void addCourse(string nama)
+    {
+        this.Course += $" , {nama} ";
+    }
+
+    public void PrintInfo()
+    {
+        Console.WriteLine($" ID\t\t = {this.Id} ");
+        Console.WriteLine($" Nama\t\t = {this.Nama} ");
+        Console.WriteLine($" NIK\t\t = {this.Nik} ");
+        Console.WriteLine($" Jenis Kelamin\t = {this.Gender} ");
+        Console.WriteLine($" Kursus\t\t = {this.Course} ");
+    }
+  }
+}
+
---
+++ Git/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Dosen
+{ 
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                Dosen dosen = new Dosen();
+
+                Console.WriteLine("Default Dosen Pengampu: ");
+                Console.WriteLine("________");
+                dosen.PrintInfo();
+
+                Console.WriteLine("\nMasukkan Nama dan NIK Dosen baru: ");
+                Console.WriteLine("____________");
+                dosen.attDosen(Console.ReadLine(), Convert.ToInt32(Console.ReadLine()));
+                dosen.PrintInfo();
+
+                Console.WriteLine("\nMasukkan Mata Kuliah Baru:");
+                Console.WriteLine("__________");
+                dosen.addCourse(Console.ReadLine());
+                dosen.PrintInfo();
+
+                Console.ReadLine();
+            }
+
+        }
+    }
